57bd1ef3686172158bf158ce0304366414324e25
rogerjeasy <rogerjeasy@gmail.com>
2024-06-16 16:16:55 +0200

    improved the content's meta data

--- src/components/views/ChatBotView.js
+++ src/components/views/ChatBotView.js
@@ -19,7 +19,7 @@ const ChatBotComponent = () => {
       setLoading(true);
 
       try {
-        const response = await fetch('https://mycv-12rg.onrender.com/api/chat', {
+        const response = await fetch('http://localhost:5000/api/chat', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
