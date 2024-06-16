ae1654a89406849fc60719fb96b8fd3d177c1137
rogerjeasy <rogerjeasy@gmail.com>
2024-06-16 17:18:43 +0200

    updated server url

--- src/components/views/ChatBotView.js
+++ src/components/views/ChatBotView.js
@@ -19,7 +19,8 @@ const ChatBotComponent = () => {
       setLoading(true);
 
       try {
-        const response = await fetch('http://localhost:5000/api/chat', {
+        // const response = await fetch('http://localhost:5000/api/chat', {
+          const response = await fetch('https://mycv-12rg.onrender.com/api/chat', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
